9f7e7cb2991698844d739773d0ccf682eef3032c
himanshumistri <mistrihimanshu@gmail.com>
2023-04-12 14:21:03 +0530

    1.Added error log support.

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,9 +1,6 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -19,22 +16,30 @@ import repository.ReadAwsData
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
+    var error by remember { mutableStateOf("") }
     val coroutineScope = rememberCoroutineScope()
     MaterialTheme {
-            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
-                Row(modifier = Modifier.width(200.dp)) {
-                    Button(onClick = {
-                        text = "Fetch Data"
-                        coroutineScope?.launch {
-                            ReadAwsData.listBucketObjects("test-docs")
-                        }
 
-                    }) {
-                        Text(text)
+            Column {
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+                    Row(modifier = Modifier.width(200.dp)) {
+                        Button(onClick = {
+                            text = "Fetch Data"
+                            coroutineScope?.launch {
+                                ReadAwsData.listBucketObjects("test-docs", onResult = {
+                                    println("MSG is $it")
+                                    error = error.plus(it)
+                                })
+                            }
+
+                        }) {
+                            Text(text)
+                        }
                     }
                 }
-
+                Text(text = error)
             }
+
         }
 }
 
--- src/jvmMain/kotlin/repository/ReadAwsData.kt
+++ src/jvmMain/kotlin/repository/ReadAwsData.kt
@@ -4,19 +4,22 @@ import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.model.ListObjectsRequest
 import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.auth.awscredentials.CredentialsProvider
-import kotlinx.coroutines.CoroutineScope
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
 
 object ReadAwsData {
 
 
     suspend fun listBucketObjects(
-        bucketName: String
+        bucketName: String ,onResult:(String) -> Unit
     ){
 
         val request = ListObjectsRequest {
             bucket = bucketName
         }
 
+        onResult.invoke("Request Created \n")
         val cred = object : CredentialsProvider {
             override suspend fun getCredentials(): Credentials {
 
@@ -24,11 +27,20 @@ object ReadAwsData {
             }
         }
 
-        S3Client {region="ap-south-1";credentialsProvider=cred}.use { s3 ->
-            val response = s3.listObjects(request)
-
-            println("AWS Read Status is ${response.contents?.size} More is ${response.name}")
+        try {
+            S3Client {region="ap-south-1";credentialsProvider=cred}.use { s3 ->
+                val response = s3.listObjects(request)
+                onResult.invoke("AWS Read Status is ${response.contents?.size} More is ${response.name} \n")
+                println("AWS Read Status is ${response.contents?.size} More is ${response.name}")
+            }
+        }catch (exception: Exception){
+            val out = ByteArrayOutputStream()
+            exception.printStackTrace(PrintStream(out))
+            val str = String(out.toByteArray())
+            exception.printStackTrace()
+            onResult.invoke("\n $str")
         }
+
 
     }
 
